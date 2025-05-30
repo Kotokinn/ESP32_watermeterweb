543774bab65eccde4f8ec732fc52b245cd82f23f
Kotokinn <126090145+Kotokinn@users.noreply.github.com>
2025-05-30 17:06:46 +0700

    Auto commit

--- src/main.cpp
+++ src/main.cpp
@@ -402,7 +402,7 @@ const char html_page[] PROGMEM = R"rawliteral(
                 .then(data => {
                     document.getElementById('net-status').textContent ="đăng kí mạng...." + data.net;
                     document.getElementById('serial-status').textContent ="kết nối serial...." + data.serial;
-                    document.getElementById('image-status').textContent = data.image;
+                    document.getElementById('image-status').textContent = "kiểm tra gửi ảnh.... data.image;
                     document.getElementById('disconnect-status').textContent = data.disconnect;
                 })
                 .catch(err => {
