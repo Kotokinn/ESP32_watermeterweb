7e6bf973803753619f76b867d8a3ea52d0c51a42
Kotokinn <126090145+Kotokinn@users.noreply.github.com>
2025-05-30 16:36:38 +0700

    Auto commit

--- src/main.cpp
+++ src/main.cpp
@@ -392,8 +392,8 @@ const char html_page[] PROGMEM = R"rawliteral(
 
         const btn_check = document.querySelector('.btn-check');
         btn_check.addEventListener('click', () => {
-            fetch('/') // replace with actual ESP IP
-                .then(res => res.json())
+            //fetch('/') // replace with actual ESP IP
+                /.then(res => res.json())
                 .then(data => {
                     // Update each status line
                     document.getElementById('net-status').textContent = "aaaa";
