620e7aba3a3ea36026ff0eead7b3c4fe60c6db7d
Kotokinn <126090145+Kotokinn@users.noreply.github.com>
2025-05-30 16:18:15 +0700

    Auto commit

--- src/main.cpp
+++ src/main.cpp
@@ -534,13 +534,7 @@ void CheckValueExist(JsonDocument &doc, const String &input, const char *keyName
 
 void handleStartCheck(AsyncWebServerRequest *request)
 {
-    String json = R"({
-    "network": "đăng kí mạng....done.",
-    "serial": "kết nối serial....done.",
-    "image": "kiểm tra gửi ảnh....done.",
-    "disconnect": "bắt đầu ngắt kết nối trong 5 giây."
-  })";
-    Serial.printf("Ok");
+    
     
 }
 
