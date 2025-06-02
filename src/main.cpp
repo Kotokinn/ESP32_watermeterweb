0c55f04bbad52db7d564c0546f95fe15333d8b61
Kotokinn <126090145+Kotokinn@users.noreply.github.com>
2025-06-02 10:09:35 +0700

    Auto commit

--- src/main.cpp
+++ src/main.cpp
@@ -157,11 +157,6 @@ void saveToFile(const String &jsonString)
 // Function to load data from SPIFFS and return a ModelData object
 bool loadFromFile(ModelData &model) // note
 {
-    if (!SPIFFS.begin(true))
-    {
-        Serial.println("SPIFFS Mount Failed");
-        return false;
-    }
 
     File file = SPIFFS.open("/config.json", "r");
     if (!file)
