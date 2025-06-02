d7efb9d01142c58e51233836d659347d723fdb4a
Kotokinn <126090145+Kotokinn@users.noreply.github.com>
2025-06-02 13:57:46 +0700

    Auto commit

--- src/main.cpp
+++ src/main.cpp
@@ -574,7 +574,7 @@ void CheckValueExist(JsonDocument &doc, const String &input, const char *keyName
         doc[keyName] = input.c_str();
 }
 
-void Web_task(void *pvParameters); // declare 
+void Web_task(void *pvParameters); // declare void
 void setup()
 {
     Serial.begin(115200);
