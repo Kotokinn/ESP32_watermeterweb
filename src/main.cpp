95f4dc0d4f177924d237c3f16d921cb578f83f9e
Kotokinn <126090145+Kotokinn@users.noreply.github.com>
2025-06-02 16:16:50 +0700

    Auto commit

--- src/main.cpp
+++ src/main.cpp
@@ -591,6 +591,8 @@ void setup()
                   page.replace("%SDB%", model.getSDB());
                   page.replace("%IDDEVICE%", model.getIDDevice());
                   page.replace("%PDN%", model.getPDN());
+
+                  //
                   request->send(200, "text/html; charset=utf-8", page); });
 
     server.on("/data", HTTP_POST, [](AsyncWebServerRequest *request)
