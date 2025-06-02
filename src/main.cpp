0decc84ffa7c70981c8b7caf75432b5f992af7d7
Kotokinn <126090145+Kotokinn@users.noreply.github.com>
2025-06-02 13:36:36 +0700

    Auto commit

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <DNSServer.h>
 #include <ArduinoJson.h>
 #include <SPIFFS.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/semphr.h>
 
 #define CONFIGURATION_FILE "/config.json"
 
