9c09fe4035699433f170a96fa865ef89bfa4e7f1
Kotokinn <126090145+Kotokinn@users.noreply.github.com>
2025-06-02 15:55:23 +0700

    Auto commit

--- src/main.cpp
+++ src/main.cpp
@@ -623,6 +623,7 @@ void setup()
     server.on("/disconnect", HTTP_GET, [](AsyncWebServerRequest *request)
               { Serial.printf("Disconneting server");
                 request->send(200,"text/plain", "Disconneting server");
+                WiFi.disconnect();
                 server.end(); });
 
     // server.on("/status", HTTP_GET, [](AsyncWebServerRequest *request)
