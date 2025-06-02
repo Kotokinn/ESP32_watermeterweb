5a15b37780fe102cfc490c4be05c48deb0d0eb2f
Kotokinn <126090145+Kotokinn@users.noreply.github.com>
2025-06-02 11:16:40 +0700

    Auto commit

--- src/main.cpp
+++ src/main.cpp
@@ -172,9 +172,6 @@ bool loadFromFile(ModelData &model) // note
     doc["top"] = "10s";
     doc["left"] = "10s";
     doc["right"] = "10s";
-    doc["right"] = "10s";
-    doc["right"] = "10s";
-    doc["right"] = "10s";
     doc["bottom"] = "10s";
 
     StaticJsonDocument<1024> doc;
