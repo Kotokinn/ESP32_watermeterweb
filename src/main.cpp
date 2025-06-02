1c16b895b674f47b49adb38201e1a5b65a38f090
Kotokinn <126090145+Kotokinn@users.noreply.github.com>
2025-06-02 15:27:44 +0700

    Auto commit

--- src/main.cpp
+++ src/main.cpp
@@ -608,9 +608,8 @@ void setup()
         Serial.println("Received and Saved JSON: " + jsonString);
         request->send(200, "text/html","i was here"); });
 
-    server.on("/disconnect", HTTP_GET, [](AsyncWebServerRequest *request) {
-
-    });
+    server.on("/disconnect", HTTP_GET, [](AsyncWebServerRequest *request)
+              { server.end });
 
     // server.on("/status", HTTP_GET, [](AsyncWebServerRequest *request)
     //           {
