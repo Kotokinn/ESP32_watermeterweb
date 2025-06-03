ec38e6ab53aa343933de11c847aed2c543f96a4c
Kotokinn <126090145+Kotokinn@users.noreply.github.com>
2025-06-03 16:24:03 +0700

    Auto commit

--- src/main.cpp
+++ src/main.cpp
@@ -408,7 +408,7 @@ const char html_page[] PROGMEM = R"rawliteral(
 
         <form class="tab-form">
             <div id="status-output">
-                <p id="net-status"></p>
+                <p id="status"></p>
             </div>
             <div class="misterious-space"></div>
             <div class="btn-check">Disconnect web</div>
