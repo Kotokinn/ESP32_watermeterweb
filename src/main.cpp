ba43c268a3b070b65eb728f0ce2c33938df76691
Kotokinn <126090145+Kotokinn@users.noreply.github.com>
2025-06-03 16:39:46 +0700

    Auto commit

--- src/main.cpp
+++ src/main.cpp
@@ -661,18 +661,7 @@ void setup()
         0           // Core 0
     );
 
-    if (FLAGE_RUN_CHECK == 1)
-    {
-        xTaskCreatePinnedToCore(
-            Send_status_task,   // Hàm loop chạy trên Core 0
-            "Send_status_task", // Tên task
-            4096,               // Stack size
-            NULL,               // Tham số
-            1,                  // Priority
-            NULL,               // Task handle
-            0                   // Core 0
-        );
-    }
+   
 }
 // func
 
