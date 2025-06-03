c57dc4eaad28c539125cc47cc1d9ddeb7da02211
Kotokinn <126090145+Kotokinn@users.noreply.github.com>
2025-06-03 16:28:17 +0700

    Auto commit

--- src/main.cpp
+++ src/main.cpp
@@ -705,7 +705,7 @@ void Send_status_task(void *pvParameters)
 {
     const char *messages[] = {
         "Checkin SIM... ok",
-        "Connecting to MQTT... ok",
+        "Connecting ... ok",
         "Sensor reading... 23.4°C",
         "Device status... running",
         "Heartbeat... alive",
