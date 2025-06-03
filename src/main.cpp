a616f9d3167b49b41376c9713128cfab06d18b49
Kotokinn <126090145+Kotokinn@users.noreply.github.com>
2025-06-03 16:33:24 +0700

    Auto commit

--- src/main.cpp
+++ src/main.cpp
@@ -594,7 +594,7 @@ void setup()
 
                   request->send(200, "text/html; charset=utf-8", page); });
 
-                  if()
+                  if(FLAG)
 
     server.on("/data", HTTP_POST, [](AsyncWebServerRequest *request)
               {
