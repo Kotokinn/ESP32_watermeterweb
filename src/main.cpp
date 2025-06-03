eb1bb197e2e2954731e95fa18cab4af7140467eb
Kotokinn <126090145+Kotokinn@users.noreply.github.com>
2025-06-03 16:34:49 +0700

    Auto commit

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,9 @@
 #include <freertos/semphr.h>
 
 #define CONFIGURATION_FILE "/config.json"
-int FLAGE_RUN_CHECK = 0
+int FLAGE_RUN_CHECK = 0;
 
-const char *ssid = "ESP32_AP";
+    const char *ssid = "ESP32_AP";
 AsyncWebServer server(80);
 AsyncEventSource events("/events");
 DNSServer dnsServer;
