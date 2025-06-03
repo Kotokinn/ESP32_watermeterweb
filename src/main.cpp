60aec04b310c08c1386dffd167e095b5b693a6d7
Kotokinn <126090145+Kotokinn@users.noreply.github.com>
2025-06-03 15:51:40 +0700

    Auto commit

--- src/main.cpp
+++ src/main.cpp
@@ -240,9 +240,9 @@ void loadFromFile(ModelData &model) // note
 }
 
 const char html_page[] PROGMEM = R"rawliteral(
-<!DOCTYPE h="en">
-tml>
-<html lang
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
