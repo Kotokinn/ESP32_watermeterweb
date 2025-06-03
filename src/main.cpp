35bac5e961e7c39cad27aa3301f0859dfcfc57b2
Kotokinn <126090145+Kotokinn@users.noreply.github.com>
2025-06-03 16:13:31 +0700

    Auto commit

--- src/main.cpp
+++ src/main.cpp
@@ -658,12 +658,12 @@ void setup()
                 server.reset();
                 WiFi.disconnect(true); });
 
-    events.onConnect([](AsyncEventSourceClient *client)
-                     {
-        if (client->lastId()) {
-        Serial.printf("SSE Client reconnected! Last message ID that it gat is: %" PRIu32 "\n", client->lastId());
-        }
-    client->send("hello!", NULL, millis(), 1000); });
+    // events.onConnect([](AsyncEventSourceClient *client)
+    //                  {
+    //     if (client->lastId()) {
+    //     Serial.printf("SSE Client reconnected! Last message ID that it gat is: %" PRIu32 "\n", client->lastId());
+    //     }
+    // client->send("hello!", NULL, millis(), 1000); });
 
     // server.on("/status", HTTP_GET, [](AsyncWebServerRequest *request)
     //           {
