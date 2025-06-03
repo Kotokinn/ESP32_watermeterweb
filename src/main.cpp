590c7f32a01faa799228b6728f2d4e1f0b49cfa3
Kotokinn <126090145+Kotokinn@users.noreply.github.com>
2025-06-03 16:34:23 +0700

    Auto commit

--- src/main.cpp
+++ src/main.cpp
@@ -618,6 +618,8 @@ void setup()
         serializeJson(doc, jsonString);
         saveToFile(jsonString);
 
+        FLAGE_RUN_CHECK =1;
+
         Serial.println("Received and Saved JSON: " + jsonString);
         String referer = request->header("Referer");
         if (referer.length() == 0) {
